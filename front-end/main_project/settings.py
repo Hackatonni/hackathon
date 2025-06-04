a1b80a9edf25cc90ec799d2c55a5c9c7b36193a5
Abdulaileb <abdulaileb@gmail.com>
2025-06-04 09:26:23 +0200

    added some config files

--- front-end/main_project/settings.py
+++ front-end/main_project/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
